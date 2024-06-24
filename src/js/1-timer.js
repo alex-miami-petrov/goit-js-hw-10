f997153863afe5fd6100c9d734e32c8b39e3b6b0
Miami <alex.miami.petrov@gmail.com>
2024-06-24 11:31:07 +0300

    m

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -23,3 +23,52 @@ const options = {
 };
 
 flatpickr('#datetime-picker', options);
+
+let countdown;
+
+document.querySelector('[data-start]').addEventListener('click', function () {
+  document.getElementById('datetime-picker').disabled = true;
+  this.disabled = true;
+
+  const selectedDate = flatpickr.parseDate(
+    document.getElementById('datetime-picker').value,
+    'Y-m-d H:i'
+  );
+
+  countdown = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = selectedDate.getTime() - now;
+
+    if (distance <= 0) {
+      clearInterval(countdown);
+      document.querySelector('.timer').innerHTML = '00:00:00:00';
+      // Enable input and Start button when countdown stops
+      document.getElementById('datetime-picker').disabled = false;
+      document.querySelector('[data-start]').disabled = false;
+      return;
+    }
+
+    function addLeadingZero(value) {
+      return String(value).padStart(2, '0');
+    }
+
+    function convertMs(ms) {
+      // Number of milliseconds per unit of time
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+
+      // Remaining days
+      const days = Math.floor(ms / day);
+      // Remaining hours
+      const hours = Math.floor((ms % day) / hour);
+      // Remaining minutes
+      const minutes = Math.floor(((ms % day) % hour) / minute);
+      // Remaining seconds
+      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+      return { days, hours, minutes, seconds };
+    }
+  });
+});
