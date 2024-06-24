93dc0404c424ae29310a3b6da217f1bb0bd7b681
Miami <alex.miami.petrov@gmail.com>
2024-06-24 10:58:59 +0300

    m

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,2 +1,25 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate < new Date()) {
+      window.alert('Please choose a date in the future');
+
+      document.querySelector('[data-start]').disabled = true;
+    } else {
+      document.querySelector('[data-start]').disabled = false;
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
